a764dfc959d4e8cae3db318eaf1953ede3005b94
Aaron Keech <aaronkeech@gatech.edu>
2018-03-09 01:04:07 -0500

    Add working classification training task

--- model.py
+++ model.py
@@ -4,32 +4,27 @@ import numpy as np
 from ops import *
 
 class WaveNet(object):
-	def __init__(self, input_size, output_size, filter_width=2, dilation_channels=32, skip_channels=256, 
-		output_channels=256, num_layers=10, name='WaveNet', learning_rate=0.001):
+	def __init__(self, input_size, output_size, dilations, filter_width=2, dilation_channels=32, skip_channels=256, 
+		output_channels=256, name='WaveNet', learning_rate=0.001):
 
 		self.input_size = input_size
 		self.output_size = output_size
+		self.dilations = dilations
 		self.filter_width = filter_width
 		self.dilation_channels = dilation_channels
 		self.skip_channels = skip_channels
 		self.output_channels = output_channels
-		self.num_layers = num_layers
 		
 		with tf.variable_scope(name):
 			self.inputs, self.logits, self.out = self.createNetwork()
 			self.network_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
-
-
-		print(self.inputs)
-		print(self.logits)
-		print(self.out)
 
 		self.targets = tf.placeholder(tf.float32, [None, self.output_size])
 
 		labels = tf.expand_dims(self.targets, 1)
 
 
-		self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=labels))
+		self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=labels))
 
 		self.optimize = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
 
@@ -42,8 +37,8 @@ class WaveNet(object):
 
 		h = DilatedCausalConv1d(h, self.filter_width, channels=self.dilation_channels, dilation_rate=1, name='causal_conv')
 
-		for i in range(self.num_layers):
-			dilation = 2 ** i
+		for i in range(len(self.dilations)):
+			dilation = self.dilations[i]
 			name = 'dilated_conv_{}'.format(i)
 			h, skip = ResidualDilationLayer(h, kernel_size=self.filter_width, dilation_channels=self.dilation_channels, 
 				skip_channels=self.skip_channels, dilation_rate=dilation, name=name)
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import argparse
+import time
+import os
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -6,38 +8,74 @@ import matplotlib.pyplot as plt
 from data import AudioData
 from model import WaveNet
 
+#from simple_audio import generate_wave_batch
+
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--logdir', type=str, default='events/%d' % int(time.time() * 1000), help='Directory where checkpoint and summary is stored')
 	parser.add_argument('--classifier', action='store_true', help='Train a classifier wavenet')
 	parser.add_argument('--siamese', action='store_true', help='Train a siamese wavenet')
 	args = parser.parse_args()
 
 	batch_size = 1
-	num_steps = 100000
+	num_steps = 300000
 	print_steps = 100
 
+	last_checkpoint_time = time.time()
+
 	if args.classifier:
+
+		#
 
 		audio_data = AudioData()
 		num_samples = audio_data.num_samples
 		num_classes = audio_data.classes
 
-		network = WaveNet(num_samples, num_classes, output_channels=num_classes)
+		dilations = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
+                  1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+#		num_samples = 16000
+#		num_classes = 4
+
+		network = WaveNet(num_samples, num_classes, dilations, dilation_channels=32, skip_channels=128, output_channels=num_classes)
+
+		saver = tf.train.Saver()
 
 		with tf.Session() as sess:
 			sess.run(tf.global_variables_initializer())
 
-			for i in range(num_steps):
+
+			if args.logdir is not None and os.path.exists(args.logdir):
+				checkpoint_state = tf.train.get_checkpoint_state(args.logdir)
+				if checkpoint_state is not None:
+					try:
+						saver.restore(sess, checkpoint_state.model_checkpoint_path)
+						print('Restoring previous session')
+						#step_count = policy.global_step.eval(sess)
+					except (tf.errors.NotFoundError):
+						print('Could not find checkpoint at %s', checkpoint_state.model_checkpoint_path)
+
+			for global_step in range(num_steps):
 				x, y = audio_data.TrainBatch(batch_size)
+				#x, y = generate_wave_batch(batch_size, num_samples, combos=False)
 				loss = network.train(x, y)
 
-				if i % print_steps == 0:
+				if global_step % print_steps == 0:
 					labels = network.predict(x)
 					#print(loss, np.sum(np.abs(y - np.round(labels))))
-					print(loss)
-					print(y - labels)
+					print('Step: {:6d} | Loss: {:.4f} | Correct Pred: {:.4f}'.format(global_step, loss, np.sum(y * labels)))
+
+
+				# Checkpoint once per minute
+				if time.time() - last_checkpoint_time > 60:
+					if not os.path.isdir(args.logdir):
+						os.makedirs(args.logdir)
+					saver.save(sess, os.path.join(args.logdir, 'model.ckpt'), global_step)
+					last_checkpoint_time = time.time()
+
+			saver.save(sess, os.path.join(args.logdir, 'model.ckpt'), global_step)
 
 
 	if args.siamese:
